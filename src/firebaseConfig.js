98e0ba289cb4260c56a0604212874985311069bb
Jonathan Ardila <mized2009gmail.com>
2023-02-27 10:54:53 -0500

    inicio de auth

---
+++ src/firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBbfACRdJgd2l6doQz-FwBhOaWYC2a6AJQ",
+    authDomain: "vue3-2023-8389d.firebaseapp.com",
+    projectId: "vue3-2023-8389d",
+    storageBucket: "vue3-2023-8389d.appspot.com",
+    messagingSenderId: "148694761399",
+    appId: "1:148694761399:web:c9ac5ad68379fadaf9b232"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const auth = getAuth()
+
+export { auth }
---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit.prevent="handleSubmit">
+            <input type="email" name="" id="" placeholder="Ingrese Email" v-model="email">
+            <input type="password" name="" id="" placeholder="Ingrese Contraseña" v-model="password">
+            <button type="submit" :disabled="userStore.loadingUser">Acceso</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useUserStore } from '../stores/user'
+// import { useRouter } from 'vue-router'
+
+
+const userStore = useUserStore()
+
+// const router = useRouter()
+
+const email = ref('a@gmail.com')
+const password = ref('123123')
+
+const handleSubmit = async() => {
+    if(!email.value || !password.value.length > 6){
+        return alert('completar campos')
+    }
+    await userStore.loginUser(email.value, password.value)
+    // router.push('/')
+}
+</script>
